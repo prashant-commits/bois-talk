ad7dd281d72b31e8f2b75ebcc6b6b6c11fc79d14
Prashant Garg <prashant.garg@stampmyvisa.com>
2024-02-25 05:22:50 +0530

    Initialize project using Create React App Add Firebase to the project

---
+++ src/services/firebase/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAVjZo5nPil7lsvBPwI-2bGingld8nimZM',
+  authDomain: 'bois-talk.firebaseapp.com',
+  projectId: 'bois-talk',
+  storageBucket: 'bois-talk.appspot.com',
+  messagingSenderId: '296364678280',
+  appId: '1:296364678280:web:49d46f938db11e4fd5133d',
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
+// Initialize Firebase
